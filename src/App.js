88aac1963f01f86cc27551a9b2714ac1f1a5d6e7
HyeonWooGa <yeonwoopark22@gmail.com>
2022-07-07 02:49:04 +0900

    create-react-app
    + typescript depedencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <h2>Start editing to se some magic happen!</h2>
+    </div>
+  );
+}
+
+export default App;
